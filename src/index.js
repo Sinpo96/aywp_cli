1df79bc594d0ea80e4bf74971d3d364a1e3fdf96
sinpo0 <Sinpo96@163.com>
2020-12-04 21:15:18 +0800

    版本号

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const clone = require('./cloneRepository');
 
 const program = new Command();
 
-program.version('1.0.0', '-v', '--version')
-    .command('init <name>')
-    .action(async (name) => {
-        await clone(name, 'wxstore_client_test');
-    });
+// 当前版本号
+program.version('0.0.1', '-v', '--version');
+
 program.parse(process.argv);
